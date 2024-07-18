b42db77beed5070cbef2faace5dcca16bc5d897f
Luis Angel Zapata <dev.luis.zapata@gmail.com>
2024-07-17 19:39:11 -0600

    feat: readed complete page

--- GenericScrapper.py
+++ GenericScrapper.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ScrollOrigin
+
 import time
 
 class GenericScrapper:
@@ -18,27 +20,29 @@ class GenericScrapper:
 
     def perform_action(self, element_selector, action_type, value=None):
         wait = WebDriverWait(self.driver, 20)
-        element = wait.until(EC.element_to_be_clickable(element_selector))
-
+        if action_type == 'click' or action_type == 'input':
+            element = wait.until(EC.element_to_be_clickable(element_selector))
+   
         if action_type == 'click':
             element.click()
         elif action_type == 'input' and value:
             element.send_keys(value)
         elif action_type == 'scroll':
-            target = self.driver.find_element(By.CLASS_NAME, "element_selector")
-            delta_y = target.rect["y"]
-            delta_y = int(delta_y)
-            time.sleep(1)
+            target = self.driver.find_element(element_selector, value)
+            scroll_origin = ScrollOrigin.from_element(target, 0, -50)
             ActionChains(self.driver)\
-                    .scroll_by_amount(0, delta_y)\
-                    .perform()
+                .scroll_from_origin(scroll_origin, 0, 200)\
+                .perform()
+            time.sleep(2)
+        elif action_type == 'select':
+            return self.select_elements(element_selector, value)
         else:
             raise ValueError('Acción no soportada')
         
         time.sleep(5)
 
-    def select_elements(self, element_selector):
-        return self.driver.find_elements(*element_selector)
+    def select_elements(self, element_selector, value):
+        return self.driver.find_elements(element_selector, value)
 
     def get_data(self):
         return self.data
--- main.py
+++ main.py
@@ -20,3 +20,24 @@ if __name__ == "__main__":
     selector = (getattr(By, "CLASS_NAME"), "search--submit--2VTbd-T")
     scraper.perform_action(selector, "click")
 
+    #Seleccionar los productos
+    while True:
+        selector = getattr(By, "CLASS_NAME")
+        products = scraper.perform_action(selector, "select", "multi--titleText--nXeOvyr")
+
+        selector = getattr(By, "CLASS_NAME")
+        scraper.perform_action(selector, "scroll", "lazy-load")
+        print("Scrolling...")
+
+        #Verificar si siguen elementos sin cargar
+        selector = getattr(By, "CLASS_NAME")
+        lazy = scraper.perform_action(selector, "select", "lazy-load")
+        print("Lazy elements: ", len(lazy))
+
+        #cuando llegue al final de la pagina se rompe el ciclo
+        if len(lazy) == 0:
+            break
+           
+
+    for product in products:
+        print(product.text)
